87f5efe8b4ede2bd95a74b92ebd7e89f20dfa707
Brian Lee <blee@satstack.cloud>
2023-07-05 19:03:12 -0700

    Started rework of docker playbooks. Found a great new role.

--- ansible/playbooks/docker/chesnaught/scripts/generate-passwords.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-set -x
-PASS_PATH=satstack.net/chesnaught
-pass generate -n ${PASS_PATH}/SAMOURAI_API
-pass generate -n ${PASS_PATH}/SAMOURAI_ADMIN
-pass generate -n ${PASS_PATH}/SAMOURAI_JWT
-pass generate -n ${PASS_PATH}/SAMOURAI_MARIADB
-pass generate -n ${PASS_PATH}/BITCOIND_RPC
